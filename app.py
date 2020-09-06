02118c1ec6ef1f7e822b22ed36ec033e5155b28d
whxcer <whxcer@outlook.com>
2020-09-06 13:35:43 +0800

    添加数据库模型并使用它来操作数据库的数据

--- app.py
+++ app.py
@@ -1,9 +1,59 @@
 from flask import Flask, render_template, url_for
+import os
+import sys
+from flask_sqlalchemy import SQLAlchemy     # 导入扩展类
+import click
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+
+# 在扩展类实例化之前设置好配置项
+WIN = sys.platform.startswith('win')
+if WIN:
+    # 如果是 Windows 系统，则使用三个斜线
+    prefix = 'sqlite:///'
+else:
+    # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False            # 关闭对模型修改的监控
+
+
+# 扩展 初始化 操作
+db = SQLAlchemy(app)
+
+
+# 创建数据库模型
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+# 自定义命令
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+
+    if drop:
+        db.drop_all()
+    db.create_all()
+
+    # 操作完成后显示提示信息
+    click.echo('Initialized database.')
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
     name = 'Whxcer'
     movies = [
         {'title': '两杆大烟枪', 'year': '1998'},
@@ -24,4 +74,20 @@ def index():
         {'title': '流浪地球', 'year': '2019'},
     ]
 
-    return render_template('index.html', name=name, movies=movies)
+    user = User(name=name)
+    db.session.add(user)
+
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+@app.route('/')
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+
+    return render_template('index.html', user=user, movies=movies)
