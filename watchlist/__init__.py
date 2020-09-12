e35e980215eba1550393ea03ad3ac172082a9da9
whxcer <whxcer@outlook.com>
2020-09-12 10:07:50 +0800

    部署前的准备

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -17,9 +17,9 @@ else:
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False            # 关闭对模型修改的监控
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 
 
 # 扩展 初始化 操作
